10d5b6e2cad576728b5121618c6b4090acd1294c
Vikramjeet Singh <30014464@student.toiohomai.ac.nz>
2019-09-04 14:49:10 +1200

    groupwork

---
+++ TN1CLASS/TN1CLASS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TN1CLASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
