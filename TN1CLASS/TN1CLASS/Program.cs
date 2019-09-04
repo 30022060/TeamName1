3c5310ed0e49ab00ec9c8e71d9ad282b4a752a9b
Vikramjeet Singh <30014464@student.toiohomai.ac.nz>
2019-09-04 15:38:08 +1200

    groupwork

--- TN1CLASS/TN1CLASS/Program.cs
+++ TN1CLASS/TN1CLASS/Program.cs
@@ -8,6 +8,43 @@ namespace TN1CLASS
 {
     class Program
     {
+        class Employee
+        {
+            private string bank;
+            private int ird;
+            private double pay;
+            private int hrs;
+            private string name;
+            private int age;
+
+            public string Bank { get { return bank; } set { bank = value; } }
+            public int Ird { get { return ird; } set { ird = value; } }
+            public double Pay { get { return pay; } set { pay = value; } }
+            public int Hrs { get { return hrs; } set { hrs = value; } }
+            public string Name { get { return name; } set { name = value; } }
+            public int Age { get { return age; } set { age = value; } }
+
+            public Employee()
+            {
+
+            }
+            public Employee(string _bank, int _ird, double _pay, int _hrs, string _name, int _age)
+            {
+                Bank = _bank;
+                Ird = _ird;
+                Pay = _pay;
+                Hrs = _hrs;
+                Name = _name;
+                Age = _age;
+            }
+            public string EmployeeDtls()
+            {
+                return $"Name: {Name}\nAge: {Age}\nIrd: {Ird}\nBank: {Bank}\nPay: {Pay}\nHrs: {Hrs}";
+
+            }
+
+
+        }
         static void Main(string[] args)
         {
         }
